076bafad229c82807dcf5669811b9da5ce0a976f
JoeYu0524 <joe@sperax.io>
2021-08-19 18:58:20 -0400

    add comments on VaultCore.sol (unfinished)

--- contracts/vault/VaultCore.sol
+++ contracts/vault/VaultCore.sol
@@ -3,6 +3,9 @@
 // Note: assuming when exponentWithPrec >= 2^32, toReturn >= swapFeePresion () (TO-DO: work out the number)
 //TO-DO: AAVE in progress
 //TO-DO: deal with assetDefaultStrategies
+//TO-DO: what happen when we redeem aTokens
+//TO-DO: whether _redeem needs "SafeTransferFrom" and how
+//TO-DO: check all user inputs, especially mintView
 pragma solidity ^0.6.12;
 
 import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
@@ -28,29 +31,30 @@ contract VaultCore is Initializable, OwnableUpgradeable {
 	using MyMath for uint;
 	using StableMath for uint;
 
-	bool public mintRedeemAllowed;
-	bool public capitalAllowed;
+	bool public mintRedeemAllowed;	// if false, no USDs can be minted or burnt
+	bool public capitalAllowed;		// if false, no collaterals can be reinvested
 
 	bool public swapfeeInAllowed;
 	bool public swapfeeOutAllowed;
 
-	mapping(address => uint) public supportedCollatAmount;
-	mapping(address => uint) public supportedCollat;
-	mapping(address => address) public assetDefaultStrategies;
-	address[] allCollat;
-	address[] allStrategies;
-	mapping (address => mapping (address => uint)) public strategiesAllocatedAmt;
+	mapping(address => uint) public supportedCollatAmount;	// the total amount of some supported collateral users have staked 	
+	mapping(address => uint) public supportedCollat;		// if it is 1, the collateral is supported; else it is 0, it is not supported
+	mapping(address => address) public assetDefaultStrategies;	// the corresponding strategy contract address to a supported collateral token (currently, they are all AAVE)
+	address[] allCollat;	// the list of all supported collaterals
+	address[] allStrategies;	// the list of all strategy addresses
+	mapping (address => mapping (address => uint)) public strategiesAllocatedAmt; // the amount of a specific collateral re-invested by a chosen strategy
 
 	address public SPATokenAddr;
 	address public oracleAddr;
-	address public collaValut;
-	address public SPAValut;
-	address public USDsFeeValut;
-	address public USDsYieldValut;
+	address public collaValut;	// the contract address that stores all collaterals
+								// WARNING: currently, this address is the same as the address of VaultCore.sol
+	address public SPAValut;	
+	address public USDsFeeValut;	// the account that stores all swapping fees
+	address public USDsYieldValut;	// TO DELETE
 
 	uint public startBlockHeight;
 
-
+	// the following constant economic parameters are subject to changes
 	uint public constant chi_alpha = 513;
 	uint public constant chi_alpha_Prec = 10**12;
 	uint public constant chiPrec = chi_alpha_Prec;
@@ -114,42 +118,73 @@ contract VaultCore is Initializable, OwnableUpgradeable {
 
 
 	// ADMIN
-
+	/**
+	 * @dev check if USDs mint & redeem are both allowed
+	 */
 	modifier whenMintRedeemAllowed {
 		require(mintRedeemAllowed, "Mint & redeem paused");
 		_;
 	}
-
+	/**
+	 * @dev check if re-investment of collaterals is allowed
+	 */
 	modifier whenNotCapitalPaused {
 		require(capitalAllowed, "Allocate paused");
 		_;
 	}
-
+	/**
+	 * @dev disable USDs mint & redeem
+	 */
 	function pauseMintBurn() external onlyOwner {
 		mintRedeemAllowed = false;
 	}
+	/**
+	 * @dev enable USDs mint & redeem
+	 */
 	function unpauseMintBurn() external onlyOwner {
 		mintRedeemAllowed = true;
 	}
+	/**
+	 * @dev disable collateral re-investment
+	 */
 	function pauseAllocate() external onlyOwner {
 		capitalAllowed = false;
 	}
+	/**
+	 * @dev enable collateral re-investment
+	 */
 	function unpauseAllocate() external onlyOwner {
 		capitalAllowed = true;
 	}
+	/**
+	 * @dev disable swapInFee, i.e. mint becomes free
+	 */
 	function pauseSwapInFee() external onlyOwner {
 		swapfeeInAllowed = false;
 	}
+	/**
+	 * @dev enable swapInFee, i.e. mint becomes non-free
+	 */
 	function unpauseSwapInFee() external onlyOwner {
 		swapfeeInAllowed = true;
 	}
+	/**
+	 * @dev disable swapOutFee, i.e. redeem becomes free
+	 */
 	function pauseSwapOutFee() external onlyOwner {
 		swapfeeOutAllowed = false;
 	}
+	/**
+	 * @dev enable swapOutFee, i.e. redeem becomes non-free
+	 */
 	function unpauseSwapOutFee() external onlyOwner {
 		swapfeeOutAllowed = true;
 	}
 
+	/**
+	 * @dev add a new supported collateral
+	 * @param _asset the address of the new supported collateral
+	 */
     function supportAsset(address _asset) external onlyOwner {
         require(supportedCollat[_asset] == 0, "Asset already supported");
 		supportedCollat[_asset] = 1;
@@ -166,17 +201,17 @@ contract VaultCore is Initializable, OwnableUpgradeable {
 		swapfeeInAllowed = true;
 		swapfeeOutAllowed = true;
 		capitalAllowed = true;
-		SPATokenAddr = 0x2B607b664A1012aD658b430E03603be1DC83EeCc;
+		SPATokenAddr = 0x2B607b664A1012aD658b430E03603be1DC83EeCc;	// SPA on Kovan
 
 		collaValut = address(this);
 		SPAValut = address(this);
 		USDsFeeValut = address(this);
-		USDsYieldValut = address(this);
-		supportedCollat[0xb7a4F3E9097C08dA09517b5aB877F7a917224ede] = 1;
+		USDsYieldValut = address(this);	// TO DELETE
+		supportedCollat[0xb7a4F3E9097C08dA09517b5aB877F7a917224ede] = 1;	// USDC on Kovan
 		allCollat.push(0xb7a4F3E9097C08dA09517b5aB877F7a917224ede);
-		supportedCollat[0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa] = 1;
+		supportedCollat[0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa] = 1;	// Dai on Kovan
 		allCollat.push(0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa);
-		supportedCollat[0x07de306FF27a2B630B1141956844eB1552B956B5] = 1;
+		supportedCollat[0x07de306FF27a2B630B1141956844eB1552B956B5] = 1;	// USDT on Kovan
 		allCollat.push(0x07de306FF27a2B630B1141956844eB1552B956B5);
 		USDsInstance = USDs(USDsToken_);
 		oracleAddr = oracleAddr_;
@@ -184,12 +219,17 @@ contract VaultCore is Initializable, OwnableUpgradeable {
 		startBlockHeight = block.number;
 	}
 
-
+	/**
+	 * @dev calculate the InSwap fee, i.e. fees for minting USDs
+	 * @return the InSwap fee
+	 */
 	function calculateSwapFeeIn() public returns (uint) {
+		// if InSwap fee is disabled, return 0
 		if (!swapfeeInAllowed) {
 			return 0;
 		}
 
+		// implement the formula in Section 4.3.1 of whitepaper 
 		uint priceUSDs_Average = IOracle(oracleAddr).getUSDsPrice_Average(); //TODO: change to 3 days average
 		uint precisionUSDs = IOracle(oracleAddr).USDsPricePrecision();
 		uint smallPwithPrecision = swapFee_p.mul(precisionUSDs).div(swapFee_p_Prec);
@@ -212,10 +252,17 @@ contract VaultCore is Initializable, OwnableUpgradeable {
 
 	// Note: need to be less than (2^32 - 1)
 	// Note: assuming when exponentWithPrec >= 2^32, toReturn >= swapFeePresion () (TO-DO: work out the number)
+	/**
+	 * @dev calculate the OutSwap fee, i.e. fees for redeeming USDs
+	 * @return the OutSwap fee 
+	 */
 	function calculateSwapFeeOut() public view returns (uint) {
+		// if the OutSwap fee is diabled, return 0
 		if (!swapfeeOutAllowed) {
 			return 0;
 		}
+
+		// implement the formula in Section 4.3.2 of whitepaper
 		uint USDsInOutRatio = IOracle(oracleAddr).USDsInOutRatio();
 		uint USDsInOutRatioPrecision = IOracle(oracleAddr).USDsInOutRatioPrecision();
 		if (USDsInOutRatio <= uint(12).mul(USDsInOutRatioPrecision).div(10)) {
@@ -234,7 +281,17 @@ contract VaultCore is Initializable, OwnableUpgradeable {
 			}
 		}
 	}
-	// chi_alpha_Prec = chiPrec
+
+
+	// note: chi_alpha_Prec = chiPrec
+	/**
+	 * @dev calculate chiTarget by the formula in section 2.2 of the whitepaper
+	 * @param chiInit_ the initial value of chi
+	 * @param blockPassed the number of blocks that have passed since USDs is launched, i.e. "Block Height"
+	 * @param priceUSDs the price of USDs, i.e. "USDs Price"
+	 * @param precisionUSDs the precision used in the variable "priceUSDs"
+	 * @return the value of chiTarget
+	 */
 	function chiTarget(uint chiInit_, uint blockPassed, uint priceUSDs, uint precisionUSDs) public pure returns (uint chiTarget_) {
 		uint chiAdjustmentA = blockPassed.mul(chiPrec).mul(chi_alpha).div(chi_alpha_Prec);
 		uint chiAdjustmentB;
@@ -249,6 +306,10 @@ contract VaultCore is Initializable, OwnableUpgradeable {
 		(, chiTarget_) = afterB.trySub(chiAdjustmentA);
 	}
 
+	/**
+	 * @dev calculate chiMint
+	 * @return chiMint, i.e. chiTarget, since they share the same value 
+	 */
 	function chiMint() public returns (uint)  {
 		uint priceUSDs = uint(IOracle(oracleAddr).getUSDsPrice());
 		uint precisionUSDs = IOracle(oracleAddr).USDsPricePrecision();
@@ -256,11 +317,17 @@ contract VaultCore is Initializable, OwnableUpgradeable {
 		return chiTarget(chiInit, blockPassed, priceUSDs, precisionUSDs);
 	}
 
+	/**
+	 * @dev calculate chiRedeem based on the formula at the end of section 2.2
+	 * @return chiRedeem
+	 */
 	function chiRedeem() public returns (uint chiRedeem_) {
+		// calculate chiTarget
 		uint priceUSDs = uint(IOracle(oracleAddr).getUSDsPrice());
 		uint precisionUSDs = IOracle(oracleAddr).USDsPricePrecision();
 		uint blockPassed = uint(block.number).sub(startBlockHeight);
 		uint chiTarget_ = chiTarget(chiInit, blockPassed, priceUSDs, precisionUSDs);
+		// calculate chiRedeem
 		uint collateralRatio_ = collateralRatio();
 		if (chiTarget_ > collateralRatio_) {
 			chiRedeem_ = chiTarget_.add(chi_gamma.mul(chiTarget_ - collateralRatio_).div(chi_gamma_Prec));
@@ -270,6 +337,11 @@ contract VaultCore is Initializable, OwnableUpgradeable {
 
 	}
 
+	/**
+	 * @dev mint USDs by entering USDs amount
+	 * @param collaAddr the address of user's chosen collateral
+	 * @param USDsMintAmt the amount of USDs to be minted
+	 */
 	function mintWithUSDs(address collaAddr, uint USDsMintAmt)
 		public
 		whenMintRedeemAllowed
@@ -279,6 +351,11 @@ contract VaultCore is Initializable, OwnableUpgradeable {
 		_mint(collaAddr, USDsMintAmt, 0);
 	}
 
+	/**
+	 * @dev mint USDs by entering SPA amount
+	 * @param collaAddr the address of user's chosen collateral
+	 * @param SPAAmt the amount of SPA to burn
+	 */
 	function mintWithSPA(address collaAddr, uint SPAAmt)
 		public
 		whenMintRedeemAllowed
@@ -288,6 +365,11 @@ contract VaultCore is Initializable, OwnableUpgradeable {
 		_mint(collaAddr, SPAAmt, 1);
 	}
 
+	/**
+	 * @dev mint USDs by entering collateral amount (excluding ETH)
+	 * @param collaAddr the address of user's chosen collateral
+	 * @param CollaAmt the amount of collateral to stake
+	 */
 	function mintWithColla(address collaAddr, uint CollaAmt)
 		public
 		whenMintRedeemAllowed
@@ -297,6 +379,10 @@ contract VaultCore is Initializable, OwnableUpgradeable {
 		_mint(collaAddr, CollaAmt, 2);
 	}
 
+	/**
+	 * @dev mint USDs by ETH
+	 * note: this function needs changes when USDs is deployed on other blockchain platform
+	 */
 	function mintWithEth() public payable whenMintRedeemAllowed {
     require(msg.value > 0, "Need to pay Ether");
 		_mint(address(0), msg.value, 3);
@@ -304,6 +390,15 @@ contract VaultCore is Initializable, OwnableUpgradeable {
 
 	//View functions
 
+	/**
+	 * @dev view related quantities for minting USDs with USDs amount
+	 * @param collaAddr the address of user's chosen collateral
+	 * @param USDsMintAmt the amount of USDs to mint
+	 * @return  the amount of SPA to burn
+	 *			the amount of collateral to stake
+	 *			the amount of USDs to mint
+	 *			the amount of Inswapfee to pay
+	 */
 	function mintWithUSDsView(address collaAddr, uint USDsMintAmt)
 		public returns (uint SPABurnAmt, uint collaDepAmt, uint USDsAmt, uint swapFeeAmount)
 	{
@@ -311,7 +406,15 @@ contract VaultCore is Initializable, OwnableUpgradeable {
 		require(USDsMintAmt > 0, "Amount needs to be greater than 0");
 		(SPABurnAmt, collaDepAmt, USDsAmt, swapFeeAmount) = mintView(collaAddr, USDsMintAmt, 0);
 	}
-
+	/**
+	 * @dev view related quantities for minting USDs with SPA amount to burn
+	 * @param collaAddr the address of user's chosen collateral
+	 * @param USDsMintAmt the amount of SPAs to burn
+	 * @return  the amount of SPA to burn
+	 *			the amount of collateral to stake
+	 *			the amount of USDs to mint
+	 *			the amount of Inswapfee to pay
+	 */
 	function mintWithSPAView(address collaAddr, uint SPAAmt)
 		public returns (uint SPABurnAmt, uint collaDepAmt, uint USDsAmt, uint swapFeeAmount)
 	{
@@ -319,7 +422,15 @@ contract VaultCore is Initializable, OwnableUpgradeable {
 		require(SPAAmt > 0, "Amount needs to be greater than 0");
 		(SPABurnAmt, collaDepAmt, USDsAmt, swapFeeAmount)  = mintView(collaAddr, SPAAmt, 1);
 	}
-
+	/**
+	 * @dev view related quantities for minting USDs with collateral amount to stake (excluding ETH)
+	 * @param collaAddr the address of user's chosen collateral
+	 * @param USDsMintAmt the amount of collateral to stake
+	 * @return  the amount of SPA to burn
+	 *			the amount of collateral to stake
+	 *			the amount of USDs to mint
+	 *			the amount of Inswapfee to pay
+	 */
 	function mintWithCollaView(address collaAddr, uint CollaAmt)
 		public returns (uint SPABurnAmt, uint collaDepAmt, uint USDsAmt, uint swapFeeAmount)
 	{
@@ -328,10 +439,19 @@ contract VaultCore is Initializable, OwnableUpgradeable {
 		(SPABurnAmt, collaDepAmt, USDsAmt, swapFeeAmount) = mintView(collaAddr, CollaAmt, 2);
 	}
 
-	// SPABurnAmt precision: 10^18
-	// collaDepAmt precision: 10^collaAddrDecimal
-	// USDsAmt precision: 10^18
-	// swapFeeAmount precision: swapFeePresion
+	/**
+	 * @dev view related quantities for minting USDs with ETH
+	 * @param CollaAmt the amount of ETH that the user stakes for minting USDs
+	 * @return  the amount of SPA to burn
+	 *			the amount of collateral to stake
+	 *			the amount of USDs to mint
+	 *			the amount of Inswapfee to pay
+	 *
+	 * SPABurnAmt precision: 10^18
+	 * collaDepAmt precision: 10^collaAddrDecimal
+	 * USDsAmt precision: 10^18
+	 * swapFeeAmount precision: swapFeePresion
+	 */
 	function mintWithEthView(uint CollaAmt)
 		public returns (uint SPABurnAmt, uint collaDepAmt, uint USDsAmt, uint swapFeeAmount)
 	{
@@ -339,11 +459,26 @@ contract VaultCore is Initializable, OwnableUpgradeable {
 		(SPABurnAmt, collaDepAmt, USDsAmt, swapFeeAmount) = mintView(address(0), CollaAmt, 3);
 	}
 
+	/**
+	 * @dev the general mintView function that display all related quantities based on mint types
+	 *		valueType = 0: mintWithUSDs
+	 *		valueType = 1: mintWithSPA
+	 *		valueType = 2: mintWithColla
+	 *		valueType = 3: mintWithETH
+	 * @param collaAddr the address of user's chosen collateral
+	 * @param valueAmt the amount of user input whose specific meaning depends on valueType
+	 * @param valueType the type of user input whose interpretation is listed above in @dev
+	 * @return  the amount of SPA to burn
+	 *			the amount of collateral to stake
+	 *			the amount of USDs to mint
+	 *			the amount of Inswapfee to pay
+	 */
 	function mintView(
 		address collaAddr,
 		uint valueAmt,
 		uint8 valueType
 	) public returns (uint SPABurnAmt, uint collaDepAmt, uint USDsAmt, uint swapFeeAmount) {
+		// obtain the price and pecision of the collateral
 		uint priceColla = 0;
 		uint precisionColla = 0;
 		if (valueType == 3) {
@@ -353,59 +488,63 @@ contract VaultCore is Initializable, OwnableUpgradeable {
 			priceColla = IOracle(oracleAddr).collatPrice(collaAddr);
 			precisionColla = IOracle(oracleAddr).collatPricePrecision(collaAddr);
 		}
+		// obtain other necessary data
 		uint priceSPA = IOracle(oracleAddr).getSPAPrice();
 		uint precisionSPA = IOracle(oracleAddr).SPAPricePrecision();
 		uint swapFee = calculateSwapFeeIn();
 		uint collaAddrDecimal = uint(ERC20Upgradeable(collaAddr).decimals());
 
-
-		if (valueType == 0) {
+		if (valueType == 0) { // when mintWithUSDs
+			// calculate USDsAmt
 			USDsAmt = valueAmt;
+			// calculate SPABurnAmt
 			SPABurnAmt = USDsAmt.mul(chiPrec - chiMint()).mul(precisionSPA).div(priceSPA.mul(chiPrec));
 			if (swapFee > 0) {
 				SPABurnAmt = SPABurnAmt.add(SPABurnAmt.mul(swapFee).div(swapFeePresion));
 			}
-
-			//Deposit collaeral
+			// calculate collaDepAmt
 			uint collaDepAmt_18 = USDsAmt.mul(chiMint()).mul(precisionColla).div(chiPrec.mul(priceColla));
 			collaDepAmt = collaDepAmt_18.div(10**(uint(18).sub(collaAddrDecimal)));
 			if (swapFee > 0) {
 				collaDepAmt = collaDepAmt.add(collaDepAmt.mul(swapFee).div(swapFeePresion));
 			}
-
+			// calculate swapFeeAmount
 			swapFeeAmount = USDsAmt.mul(swapFee).div(swapFeePresion);
-		} else if (valueType == 1) {
-			SPABurnAmt = valueAmt;
 
+		} else if (valueType == 1) { // when mintWithSPA
+			// calculate SPABurnAmt
+			SPABurnAmt = valueAmt;
+			// calculate USDsAmt
 			USDsAmt = SPABurnAmt;
 			if (swapFee > 0) {
 				USDsAmt = USDsAmt.mul(swapFeePresion).div(swapFeePresion.add(swapFee));
 			}
 			USDsAmt = USDsAmt.mul(chiPrec).mul(priceSPA).div(precisionSPA.mul(chiPrec - chiMint()));
-
-			//Deposit collaeral
+			// calculate collaDepAmt
 			uint collaDepAmt_18 = USDsAmt.mul(chiMint()).mul(precisionColla).div(chiPrec.mul(priceColla));
 			collaDepAmt = collaDepAmt_18.div(10**(uint(18).sub(collaAddrDecimal)));
 			if (swapFee > 0) {
 				collaDepAmt = collaDepAmt.add(collaDepAmt.mul(swapFee).div(swapFeePresion));
 			}
-
+			// calculate swapFeeAmount
 			swapFeeAmount = USDsAmt.mul(swapFee).div(swapFeePresion);
-		} else if (valueType == 2 || valueType == 3) {
-			collaDepAmt = valueAmt;
-			USDsAmt = collaDepAmt;
 
+		} else if (valueType == 2 || valueType == 3) { // when mintWithColla or mintWithETH
+			// calculate collaDepAmt
+			collaDepAmt = valueAmt;
+			// calculate USDsAmt
+			USDsAmt = collaDepAmt;
 			if (swapFee > 0) {
 				USDsAmt = USDsAmt.mul(swapFeePresion).div(swapFeePresion.add(swapFee));
 			}
 			uint CollaDepAmt_18 = USDsAmt.mul(10**(uint(18).sub(collaAddrDecimal)));
 			USDsAmt = CollaDepAmt_18.mul(chiPrec.mul(priceColla)).div(precisionColla).div(chiMint());
-
+			// calculate SPABurnAmt
 			SPABurnAmt = USDsAmt.mul(chiPrec - chiMint()).mul(precisionSPA).div(priceSPA.mul(chiPrec));
 			if (swapFee > 0) {
 				SPABurnAmt = SPABurnAmt.add(SPABurnAmt.mul(swapFee).div(swapFeePresion));
 			}
-
+			// calculate swapFeeAmount
 			swapFeeAmount = USDsAmt.mul(swapFee).div(swapFeePresion);
 		}
 	}
