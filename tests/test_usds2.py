15b9531278fc90cb16b9c9e871dcd0955ec0a347
Rui Maximo <rmaximo@lync-solutions.com>
2021-11-05 18:42:12 -0700

    fix test_usds2.py

--- tests/test_usds2.py
+++ tests/test_usds2.py
@@ -9,7 +9,7 @@ from brownie.test import given, strategy
 
 @given(amount=strategy('uint256', min_value=1, max_value=2**256-1))
 def test_valid_mint(sperax, accounts, amount):
-    (proxy_admin, spa, usds2, vault_core_tools, vault_proxy) = sperax
+    (proxy_admin, spa, usds_proxy, vault_core_tools, vault_proxy) = sperax
 
     print('amount: ', amount)
     first_owner = accounts[3]
@@ -18,36 +18,36 @@ def test_valid_mint(sperax, accounts, amount):
     failed_owner = accounts[6]
     third_owner = accounts[7]
     # mint stablecoin
-    usds2.mint(first_owner, amount, {'from': vault.address})
-    assert usds2.balanceOf(first_owner) == amount
-    assert usds2.totalSupply() == amount
+    usds_proxy.mint(first_owner, amount, {'from': vault_proxy.address})
+    assert usds_proxy.balanceOf(first_owner) == amount
+    assert usds_proxy.totalSupply() == amount
 
     # approve transfer 
-    usds2.approve(approver, amount, {'from': first_owner})
-    txn = usds2.transferFrom(first_owner, second_owner, amount, {'from': approver})
+    usds_proxy.approve(approver, amount, {'from': first_owner})
+    txn = usds_proxy.transferFrom(first_owner, second_owner, amount, {'from': approver})
     assert True == txn.return_value
     assert txn.events['Transfer']['from'] == first_owner 
     assert txn.events['Transfer']['to'] == second_owner
     assert txn.events['Transfer']['value'] == amount
-    assert usds2.balanceOf(second_owner) == amount
-    assert usds2.totalSupply() == amount
+    assert usds_proxy.balanceOf(second_owner) == amount
+    assert usds_proxy.totalSupply() == amount
 
     # first_owner no longer owns the tokens. try to transfer
     with brownie.reverts():
-        usds2.transfer(failed_owner, amount, {'from': first_owner})
+        usds_proxy.transfer(failed_owner, amount, {'from': first_owner})
     
     # new owner transfers stablecoin
-    usds2.transfer(third_owner, amount, {'from': second_owner})
-    assert usds2.balanceOf(third_owner) == amount
-    assert usds2.totalSupply() == amount
+    usds_proxy.transfer(third_owner, amount, {'from': second_owner})
+    assert usds_proxy.balanceOf(third_owner) == amount
+    assert usds_proxy.totalSupply() == amount
 
     # amount of stablecoins to burn
     amount_to_burn = amount - (amount // 2)
     print('number of tokens to burn: ', amount_to_burn)
     # stablecoin owner cannot burn their own tokens
     with brownie.reverts():
-        usds2.burn(third_owner, amount_to_burn, {'from': second_owner})
+        usds_proxy.burn(third_owner, amount_to_burn, {'from': second_owner})
 
     # new owner stablecoins can only be burned by vault 
-    usds2.burn(third_owner, amount_to_burn, {'from': vault.address})
-    assert usds2.totalSupply() == amount - amount_to_burn
+    usds_proxy.burn(third_owner, amount_to_burn, {'from': vault_proxy.address})
+    assert usds_proxy.totalSupply() == amount - amount_to_burn
